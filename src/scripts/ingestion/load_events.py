f17d03b58e4b7aaf0d3ac2426aff42f4c14724c2
Resh <118062429+reshbuilds@users.noreply.github.com>
2025-04-08 21:02:49 +0200

    jkpg scraping added

--- src/scripts/ingestion/load_events.py
+++ src/scripts/ingestion/load_events.py
@@ -18,8 +18,8 @@ lan_header = {
 try:
   lan_resp = requests.get(lan_url,headers=lan_header)
   if lan_resp.status_code == 200:
-    soup=BeautifulSoup(lan_resp.text,features="html.parser")
-    lan_json_data = soup.find(id="Evenemangslistning").next_sibling
+    lan_soup = BeautifulSoup(lan_resp.text,features="html.parser")
+    lan_json_data = lan_soup.find(id="Evenemangslistning").next_sibling
     lan_json_dict = json.loads(lan_json_data)
 
     events_lan_list = lan_json_dict["searchHits"]
@@ -30,13 +30,46 @@ try:
       json.dump(events_lan_list, f, ensure_ascii=False, indent=4)
     
     with open(events_lan_list_filepath, "r", encoding="utf-8") as f:
-        ev = json.load(f)
-        if events_lan_total==len(ev):
-          print(datetime.datetime.now().strftime('%d %b %Y, %H:%M')," : Event data (" + str(events_lan_total) + " events) from Jönköping Evenemangskalender has been successfully scraped and saved")
+        lan_ev = json.load(f)
+        if events_lan_total==len(lan_ev):
+          print(datetime.datetime.now().strftime('%d %b %Y, %H:%M')," : Event data (" + str(len(lan_ev)) + " events) from Jönköping Evenemangskalender has been successfully scraped and saved")
   else:
-    print("The website is down. Please try again later : ",resp.status_code)
+    print("The website is down. Please try again later : ",lan_resp.status_code)
 except Exception as e:
   print("Problems with scraping the website. Please check the following error message :", e)
 
 
-# Scrape event data from JKPG website
+# Scrape event data from Destination Jönköping's JKPG website
+
+try:
+  dj_url = "https://jkpg.com/sv/step/api/v1/public-experience-list/?strict=0&skip=10000&take=18"
+  dj_resp = requests.get(dj_url)
+  if dj_resp.status_code == 200:
+    dj_soup = BeautifulSoup(dj_resp.text,features="html.parser")
+    events_dj_total = json.loads(dj_soup.text)["total_experiences"]
+    
+    events_dj_list = []
+    for skip_val in range(0,events_dj_total+1,18):
+      dj_url_t = "https://jkpg.com/sv/step/api/v1/public-experience-list/?strict=0&skip=" + str(skip_val) + "&take=18"
+      dj_resp_t = requests.get(dj_url_t)
+      if dj_resp_t.status_code == 200:
+        dj_soup_t = BeautifulSoup(dj_resp_t.text,features="html.parser")
+        el = json.loads(dj_soup_t.text)["experiences"]
+        events_dj_list += el
+      else:
+        print("The website is down. Please try again later : ",dj_resp_t.status_code)
+        break
+      
+    events_dj_list_filepath = "src/data/raw/events_dj_list.json"
+    with open(events_dj_list_filepath, "w", encoding="utf-8") as f:
+      json.dump(events_dj_list, f, ensure_ascii=False, indent=4)
+    
+    with open(events_dj_list_filepath, "r", encoding="utf-8") as f:
+        dj_ev = json.load(f)
+        if events_dj_total==len(dj_ev):
+          print(datetime.datetime.now().strftime('%d %b %Y, %H:%M')," : Event data (" + str(len(dj_ev)) + " events) from Destination Jönköping has been successfully scraped and saved")
+  else:
+    print("The website is down. Please try again later : ",resp.status_code)
+except Exception as e:
+  print("Invalid website. Please check the following error message :", e)
+
