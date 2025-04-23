93062a068981e758fb45e5b8884f9c6609f13666
Resh <118062429+reshbuilds@users.noreply.github.com>
2025-04-23 21:31:29 +0200

    Streamlit integration

---
+++ src/scripts/frontend/get_events.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+
+from src.scripts.ingestion.load_events import *
+
+if 'gov' not in st.session_state:
+    st.session_state.gov = ""
+if 'priv' not in st.session_state:
+    st.session_state.priv = ""
+
+st.title("Get Events")
+st.subheader("")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Event loader (Municipality)",type="primary"):
+        st.session_state.gov = get_events_gov()
+    st.caption(st.session_state.gov)
+
+with col2:
+    if st.button("Event loader (Private)",type="primary"):
+        st.session_state.priv = get_events_private()
+    st.caption(st.session_state.priv)
